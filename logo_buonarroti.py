42e57aaa513a89ea2016a7df392899c2f90f8f7e
Leonardo Essam Dei Rossi <65762081+leonardodeirossi@users.noreply.github.com>
2022-01-30 17:14:52 +0100

    Esempio logo buonarroti (2)

--- logo_buonarroti.py
+++ logo_buonarroti.py
@@ -3,14 +3,14 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 X = [30, 144, 255]  # Red
-O = [255, 255, 255]  # White
+O = [0, 0, 0]  # White
 
 logo_buonarroti = [
     O, O, X, O, X, O, O, O,
     O, O, O, O, O, X, O, O,
-    O, O, O, O, X, O, O, O,
-    O, O, O, X, O, O, O, O,
-    O, O, X, O, O, O, O, O,
+    O, O, O, O, O, O, X, O,
+    O, O, O, X, O, X, O, O,
+    O, O, X, O, X, O, O, O,
     O, O, X, O, O, X, O, O,
     O, O, X, O, X, O, O, O,
     O, O, O, O, O, O, O, O
