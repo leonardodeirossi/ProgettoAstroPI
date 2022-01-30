6b3e6375a5cad05adc886c0b0b52bf75e8d2b651
Leonardo Essam Dei Rossi <65762081+leonardodeirossi@users.noreply.github.com>
2022-01-30 18:22:28 +0100

    Esempio che spegne i led

---
+++ ESEMPI/reset_led.py
@@ -0,0 +1,18 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+O = [255, 255, 255]  # Off
+
+reset_led = [
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O
+]
+
+sense.set_pixels(reset_led)
