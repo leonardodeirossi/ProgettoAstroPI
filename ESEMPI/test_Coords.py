7db0fcfd9c4eaa15529804f780a1f059fdb956ad
Leonardo Essam <leonardo.deirossi@icloud.com>
2022-02-01 08:56:18 +0100

    Update test_Coords.py

--- ESEMPI/test_Coords.py
+++ ESEMPI/test_Coords.py
@@ -1,13 +1,14 @@
 from skyfield.api import load, wgs84
+stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
+satellites = load.tle_file(stations_url)
+print('Loaded', len(satellites), 'satellites')
 ts = load.timescale()
 t = ts.utc(2014, 1, 23, 11, 18, 7)
 by_name = {sat.name: sat for sat in satellites}
 satellite = by_name['ISS (ZARYA)']
 print(satellite)
 geocentric = satellite.at(t)
-stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
-satellites = load.tle_file(stations_url)
-print('Loaded', len(satellites), 'satellites')
+
 
 lat, lon = wgs84.latlon_of(geocentric)
 print('Latitude:', lat)
