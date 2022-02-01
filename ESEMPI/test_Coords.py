5d76e81d1dfe7a85ea55f75dad96e64e2aa5b2f6
Leonardo Essam <leonardo.deirossi@icloud.com>
2022-02-01 09:04:28 +0100

    Update test_Coords.py

--- ESEMPI/test_Coords.py
+++ ESEMPI/test_Coords.py
@@ -1,6 +1,8 @@
 from skyfield.api import load, wgs84, EarthSatellite
 
+
 ts = load.timescale()
+t = ts.utc(2014, 1, 23, 11, 18, 7)
 line1 = '1 25544U 98067A   14020.93268519  .00009878  00000-0  18200-3 0  5082'
 line2 = '2 25544  51.6498 109.4756 0003572  55.9686 274.8005 15.49815350868473'
 satellite = EarthSatellite(line1, line2, 'ISS (ZARYA)', ts)
