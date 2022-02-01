ddf76652bc232b4fcb9b07ca787c4ca2397959fb
Leonardo Essam <leonardo.deirossi@icloud.com>
2022-02-01 08:52:53 +0100

    Update test_Coords.py

--- ESEMPI/test_Coords.py
+++ ESEMPI/test_Coords.py
@@ -1,5 +1,8 @@
 from skyfield.api import load, wgs84
 
+t = ts.utc(2014, 1, 23, 11, 18, 7)
+
+geocentric = satellite.at(t)
 stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
 satellites = load.tle_file(stations_url)
 print('Loaded', len(satellites), 'satellites')
