491d671196835f40b30a037a19e9c330f4ef9ff5
Leonardo Essam <leonardo.deirossi@icloud.com>
2022-02-16 18:32:40 +0100

    Test acceleromentro2

---
+++ ESEMPI/accelerometro2.py
@@ -0,0 +1,40 @@
+from sense_hat import SenseHat
+from datetime import datetime
+
+sense = SenseHat()
+
+def get_sense_data():
+    print("Temperatura: ", sense.get_temperature())
+    print("Pressione: ", sense.get_pressure())
+    print("Umidità: ", sense.get_humidity())
+    """
+  sense_data = []
+  
+  sense_data.append(sense.get_temperature())
+  sense_data.append(sense.get_pressure())
+  sense_data.append(sense.get_humidity())
+
+  #acellerometro
+  acc = sense.get_accelerometer_raw()
+  sense_data.append(acc["x"])
+  sense_data.append(acc["y"])
+  sense_data.append(acc["z"])
+
+  #bussola
+  mag = sense.get_compass_raw()
+  sense_data.append(mag["x"])
+  sense_data.append(mag["y"])
+  sense_data.append(mag["z"])
+  
+  #giroscopio
+  gyro = sense.get_gyroscope_raw()
+  sense_data.append(gyro["x"])
+  sense_data.append(gyro["y"])
+  sense_data.append(gyro["z"])
+  
+  return sense_data
+  """
+  
+  
+while True:
+	print(get_sense_data())
