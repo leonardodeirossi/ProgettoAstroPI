fa57aae9af098a11e9d9c0b9405500eb17a029fb
Leonardo Essam <leonardo.deirossi@icloud.com>
2022-02-17 15:53:00 +0100

    fix caricamento dati

--- ESEMPI/Caricamento_dati.py
+++ ESEMPI/Caricamento_dati.py
@@ -65,7 +65,7 @@ with open ('data.csv','w',newline='') as f:
   
 while True:
   data = get_sense_data()
-  dt = data[-1] - timestamp
+  dt = data[-1] - int(timestamp)
   
   if dt.seconds > delay:
     data_writer.writerow(data)
